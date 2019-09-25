367f3de4ab1066b758c847adbd1f80254ca9fb98
github username <barbara.m.washington@gmail.com>
2019-09-24 21:50:50 -0700

    add features, player folders, with index.js boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import Player from './features/player';
 
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      Hello World
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Player />
+      </div>
+    )
+  }
 }
 
 export default App;
