d26410532970e52b4dcf10547af658f6532b2305
github username <barbara.m.washington@gmail.com>
2019-09-24 22:56:27 -0700

    add player position style and image

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 import store from './config/store';
 
 ReactDOM.render(<Provider store={store}>
-  <App />, 
+  <App />
 </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
