d9175c538629cac8ada3b03cfb40aff1b1f395d2
github username <barbara.m.washington@gmail.com>
2019-09-25 19:42:10 -0700

    troubleshoot mapp layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Player from './features/player';
+import Map from '/features/map';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Player />
+        <Map />
       </div>
     )
   }
---
+++ src/features/map/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Map(props) {
+  return (
+    <div 
+      style={{
+        width: '880px',
+        height: '400px',
+        backgroundColor: 'black',
+        border: '4px solid white',
+        margin: '10px',
+      }}
+    />
+  ) 
+}
+
+export default Map;
